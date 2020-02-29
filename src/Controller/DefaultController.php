109030f52b8d45a2c378568c970b7a5a339ef089
kin29 <kin29.com@gmail.com>
2020-02-29 14:24:32 +0900

    callback_urlをpostにして、authもそっちにする

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -25,7 +25,7 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        $this->spotifyAuth->handleRequest();
+        //$this->spotifyAuth->handleRequest();
 
         $form = $this->createForm(ArtistNameListType::class, null, [
             'action' => $this->generateUrl('post'),
@@ -41,6 +41,8 @@ class DefaultController extends AbstractController
      */
     public function post()
     {
+        $this->spotifyAuth->handleRequest();
+
         return $this->render('default/post.html.twig', [
             'controller_name' => 'PostController',
         ]);
