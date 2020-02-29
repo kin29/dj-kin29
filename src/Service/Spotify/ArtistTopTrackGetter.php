a85006bff8df3c215351776195a0bb0ae25d20a9
kin29 <kin29.com@gmail.com>
2020-02-29 22:41:58 +0900

    debug

--- src/Service/Spotify/ArtistTopTrackGetter.php
+++ src/Service/Spotify/ArtistTopTrackGetter.php
@@ -50,21 +50,21 @@ class ArtistTopTrackGetter
 
         $retTracks = [];
         $retArtists = [];
-        foreach ($artistNames as $artistName) {
-            $results = $this->api->search($artistName, $type, array('limit' => 1));
-            var_dump($results);
-
-            if (count($results->artists->items) == 0) continue;
-
-            $artistId = $results->artists->items[0]->id;
-            var_dump($artistId);
-            $tracks = $this->api->getArtistTopTracks($artistId, ['country' => 'JP'])->tracks;
-            var_dump($tracks);
-            foreach ($tracks as $track) {
-                $retTracks[] = $track->id;
-            }
-            $retArtists[] = $artistName;
-        }
+//        foreach ($artistNames as $artistName) {
+//            $results = $this->api->search($artistName, $type, array('limit' => 1));
+//            var_dump($results);
+//
+//            if (count($results->artists->items) == 0) continue;
+//
+//            $artistId = $results->artists->items[0]->id;
+//            var_dump($artistId);
+//            $tracks = $this->api->getArtistTopTracks($artistId, ['country' => 'JP'])->tracks;
+//            var_dump($tracks);
+//            foreach ($tracks as $track) {
+//                $retTracks[] = $track->id;
+//            }
+//            $retArtists[] = $artistName;
+//        }
 
         return [$retTracks, $retArtists];
     }
