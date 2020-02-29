8cb90a9c78593b0a75c42d12ad51c05c0669e3b9
kin29 <kin29.com@gmail.com>
2020-03-01 01:47:17 +0900

    どうしても、create/にいく。action先だから？

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -77,12 +77,12 @@ class DefaultController extends AbstractController
                 if($data) $artistNames[] = $data;
             }
             //var_dump($artistNames);
-           $this->artistTopTrackGetter->handleRequest();
+           return new RedirectResponse($this->artistTopTrackGetter->handleRequest());
 //            list($tracks, $artists) = $this->artistTopTrackGetter->get($artistNames);
 //            var_dump($tracks);
 //            var_dump($artists);
 //
-            return $this->redirect($this->generateUrl('create_complete'));
+            //return $this->redirect($this->generateUrl('create_complete'));
             //renderは効かない
         }
 
