5a096d53d8ef6ba8bd84f2817ea9559642d8cc0f
kin29 <kin29.com@gmail.com>
2020-03-01 22:46:29 +0900

    デフォルトでプレイリスト名セットするようにしておく

--- src/Form/ArtistNameListType.php
+++ src/Form/ArtistNameListType.php
@@ -3,6 +3,7 @@
 
 namespace App\Form;
 
+use DateTime;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -23,7 +24,7 @@ class ArtistNameListType extends AbstractType
                 'attr' => [
                     'placeholder' => 'playlist name',
                 ],
-                'empty_data' => 'default name'
+                'data' => 'dj-kin29-' . (new DateTime())->format('YmdHi')
             ])
             ->add('isPrivate', CheckboxType::class, [
                 'label' => 'Private playlist',
