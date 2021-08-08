faf0c54ce4a735bf90dc95339607b977e3cb21a1
kin29 <kin29.com@gmail.com>
2021-08-08 16:11:58 +0900

    ついでに.php-cs-fixer.dist.phpを調整

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,6 +9,8 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+        'ordered_imports' => true,
         'php_unit_method_casing' => false,
     ])
     ->setFinder($finder)
